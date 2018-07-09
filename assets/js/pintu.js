1af4e62d25145c68f9d2e74f7c10fa489551edaa
bagusasnd <barisandi21@gmail.com>
2018-07-09 19:36:38 +0700

    add page detail article and list trending topic

--- assets/js/pintu.js
+++ assets/js/pintu.js
@@ -11,6 +11,7 @@ function closeNav() {
 }
 
 
+
 /* Sticky Mobile Header Function */
 
 window.onscroll = function() {stickySearchBar()};
@@ -28,3 +29,49 @@ function stickySearchBar() {
         search.classList.remove("hidden");
     }
 }
+
+
+/*Content Slide Carousel*/
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+    var multiSlides       = document.querySelector('.js_multislides');
+
+
+    function handleDotEvent(e) {
+        if (e.type === 'before.lory.init') {
+            for (var i = 0, len = dot_count; i < len; i++) {
+                var clone = dot_list_item.cloneNode();
+                dot_container.appendChild(clone);
+            }
+            dot_container.childNodes[0].classList.add('active');
+        }
+        if (e.type === 'after.lory.init') {
+            for (var i = 0, len = dot_count; i < len; i++) {
+                dot_container.childNodes[i].addEventListener('click', function(e) {
+                    dot_navigation_slider.slideTo(Array.prototype.indexOf.call(dot_container.childNodes, e.target));
+                });
+            }
+        }
+        if (e.type === 'after.lory.slide') {
+            for (var i = 0, len = dot_container.childNodes.length; i < len; i++) {
+                dot_container.childNodes[i].classList.remove('active');
+            }
+            dot_container.childNodes[e.detail.currentSlide - 1].classList.add('active');
+        }
+        if (e.type === 'on.lory.resize') {
+            for (var i = 0, len = dot_container.childNodes.length; i < len; i++) {
+                dot_container.childNodes[i].classList.remove('active');
+            }
+            dot_container.childNodes[0].classList.add('active');
+        }
+    }
+
+
+    lory(multiSlides, {
+        infinite: 1,
+        slidesToScroll: 1,
+        enableMouseEvents: true
+    });
+
+});
